a00cdcd25251463ed6c4776d55de09890e270a1d
Moost999 <geertschaffer@gmail.com>
2025-03-28 20:15:48 -0300

    cors config

--- src/main/java/com/moostdev/ChatBot/Application/Study/controllers/ChatController.java
+++ src/main/java/com/moostdev/ChatBot/Application/Study/controllers/ChatController.java
@@ -4,10 +4,12 @@ import com.moostdev.ChatBot.Application.Study.services.GroqService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ChatController {
 
